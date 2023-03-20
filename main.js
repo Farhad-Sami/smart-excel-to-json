d8b5108f5383389cd8a04c55373ed42e6ad30f79
Farhad Sami <farhadsami36@gmail.com>
2023-03-20 17:09:07 +0600

    main

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const xlsx = require('xlsx');
 
-const workbook = xlsx.readFile('/home/jib/Desktop/rethink billing/pytho/Regional Center Oct 2022.xlsx');
+const workbook = xlsx.readFile(' .xlsx file path');
 
 const all = JSON.parse(JSON.stringify([].concat(...workbook.SheetNames.map((name) => xlsx.utils.sheet_to_json(workbook.Sheets[name]))), null, 2));
 console.log(all);
